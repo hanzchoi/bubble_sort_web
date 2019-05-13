1e601e0ef0ee886dfb391f317c033a438655a766
Hanz <personabe1984@gmail.com>
2019-05-13 02:01:12 -0400

    adding button and selection container in which will contain reset button as well

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import DisplayContainer from './DisplayContainer/DisplayContainer';
-import SelectionContainerf from './SelectionContainer/SelectionContainer';
+import SelectionContainer from './SelectionContainer/SelectionContainer';
 import './App.css';
 
 
@@ -13,23 +13,17 @@ const App = () => {
   const [unsortedArrayState, setUnsortedArrayState] = useState(unsortedArray);
 
   const sort = (array) => {
-    console.log("sort method was trig")
     for (let i = 0; i < array.length; i++) {
       setTimeout(() => {
         for (let j = 1; j < array.length; j++) {
           if (array[j - 1] > array[j]) {
             [array[j - 1], array[j]] = [array[j], array[j - 1]];
             setUnsortedArrayState([...array]);
-            console.log(unsortedArrayState);
           }
         }
       }, 250 * (i + 1));
     }
   }
-
-  // useEffect(() => {
-  //   console.log("updated: ",unsortedArrayState);
-  // }, [unsortedArrayState]);
 
 
   return (
---
+++ src/SelectionContainer/SelectionContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './SelectionContainer.css';
+
+const SelectionContainer = () => {
+
+  return(
+    <div></div>
+  )
+}
+
+export default SelectionContainer
